3a7d1045221665351699319bb2f7e19a899df420
Neil Grogan <neil@grogan.ie>
2016-12-21 19:39:28 +0000

    Updated with starter app

---
+++ test/test.js
@@ -0,0 +1,27 @@
+var chai = require('chai')
+var chaiHttp = require('chai-http');
+    var expect = chai.expect;
+    var server = server = require('../app')
+
+chai.use(chaiHttp);
+
+describe('Testing Grogan Burners', function () {
+
+    it('fails, as expected', function (done) {
+        chai.request(server)
+            .get('/not-exist')
+            .end(function (err, res) {
+                expect(res).to.have.status(404);
+                done(); 
+            });
+    });
+
+    it('succeeds silently!', function (done) {
+        chai.request(server)
+            .get('/')
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+});
