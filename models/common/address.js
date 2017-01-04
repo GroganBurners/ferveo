cdb1d9f86317ec9980734b97faa4d19b220669b8
Neil Grogan <neil@grogan.ie>
2017-01-04 13:31:59 +0000

    Updated Address with Saner Defaults

--- models/common/address.js
+++ models/common/address.js
@@ -1,8 +1,7 @@
-var address = { 
-    type: String, 
-    street: String, 
-    city: String, 
-    state: String, 
+var address = {
+    street: { type: String, required: true, default: '' },
+    town: { type: String, required: false, default: '' }, 
+    county: { type: String, default: 'Co. Kilkenny' }, 
     country: { type: String, default: 'Ireland' }, 
     postalCode: { type: String, required: false, default: '' },
 }
