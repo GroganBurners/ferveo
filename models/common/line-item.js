3b7a667958eb0b0db691d7826b0b479380e988ee
Neil Grogan <neil@grogan.ie>
2017-01-04 16:12:00 +0000

    Added labour as a option for line item

--- models/common/line-item.js
+++ models/common/line-item.js
@@ -1,5 +1,6 @@
 var lineItem = { 
     desc: { type: String, required: true },
+    labour: { type: Boolean, required: false, default: false },
     vatRate: { type: Number, required: false, default: 0 },
     vatTotal: { type: Number, required: false, default: 0 },
     total: { type: Number, required: true }
