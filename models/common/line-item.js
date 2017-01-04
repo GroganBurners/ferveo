70bc8b7464f6fd88e2a8366e3595eb15ad13ee79
Neil Grogan <neil@grogan.ie>
2017-01-04 15:40:42 +0000

    Added more tests & validation to expense

--- models/common/line-item.js
+++ models/common/line-item.js
@@ -1,7 +1,7 @@
 var lineItem = { 
     desc: { type: String, required: true },
     vatRate: { type: Number, required: false, default: 0 },
-    exVatTotal: { type: Number, required: false, default: 0 },
+    vatTotal: { type: Number, required: false, default: 0 },
     total: { type: Number, required: true }
 }
 
--- models/expense.js
+++ models/expense.js
@@ -5,8 +5,14 @@ var expenseItem = require('./common/line-item')
 var expenseSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now },
     company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+    category: ['Purchases', 'Motor Fuel', 'Motor Serv/Repair', 
+    'Motor Tax/Ins', 'Van DOE', 'Public Laibility Insurance', 
+    'Advertising', 'Tele/bband', 'Training', 'Office Expenses', 
+    'Sub Contractor', 'Landfill', 'Ber Cert Fees', 'Bank Fees', 
+    'Sundry', 'Heating', 'Sponsorship', 'Equipment Hire', 'Electricity'],
     items: [expenseItem],
     totalVat: { type: Number, default: 0.00 },
+    totalExVat: { type: Number, default: 0.00 },
     total: { type: Number, default: 0.00 }
 })
 
