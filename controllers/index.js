2fef084556234a6c95eca1ba91e4a2f3b3a4b5cf
Neil Grogan <neil@grogan.ie>
2017-01-05 23:21:50 +0000

    Removed unneeded import

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,4 @@
 const BaseController = require('./base')
-var mongoose = require('mongoose')
 const Customer = require('../models/customer')
 const Price = require('../models/price')
 const EmailController = require('./email')
@@ -8,14 +7,14 @@ const AuthController = require('./auth')
 
 module.exports.Customer = class CustomerController extends BaseController {
   constructor () {
-      super(Customer, '_id')
-    }
+    super(Customer, '_id')
+  }
 }
 
 module.exports.Price = class PriceController extends BaseController {
   constructor () {
-      super(Price, '_id')
-    }
+    super(Price, '_id')
+  }
 }
 
 module.exports.Email = EmailController
