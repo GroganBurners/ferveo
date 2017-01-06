9a53b8a1333c3f14484e8e72f94667486af60fb9
Neil Grogan <neil@grogan.ie>
2017-01-06 17:19:25 +0000

    made dev server run via npm

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,5 @@ var gulp = require('gulp')
 var browserSync = require('browser-sync')
 var nodemon = require('gulp-nodemon')
 
-gulp.task('default', ['browser-sync'], function () {
-})
-
-gulp.task('browser-sync', ['nodemon'], function () {
-  browserSync.init(null, {
-    proxy: 'http://localhost:3000',
-    files: ['public/**/*.*'],
-    port: 5000
-  })
-})
-gulp.task('nodemon', function (cb) {
-  var started = false
-
-  return nodemon({
-    exec: 'node --debug',
-    script: './bin/www'
-  }).on('start', function () {
-		// to avoid nodemon being started multiple times
-		// thanks @matthisk
-    if (!started) {
-      cb()
-      started = true
-    }
-  })
+gulp.task('default', function () {
 })
