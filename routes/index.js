b7bfc0f843f192a54d18ee0b7fd254e5509504d2
Neil Grogan <neil@grogan.ie>
2017-01-06 14:36:47 +0000

    Removing unused routes

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,31 @@ const EmailController =cont.Email
 const passportConfig = require('../config/passport')
 
 module.exports = function (app) {
+
+  // API
   app.use('/api/customers', passportConfig.ensureAuthenticated, new CustomerController().route())
   app.use('/api/prices', new PriceController().route())
   app.use('/api/email', new EmailController().route())
+
+  // Ordinary web pages
+  app.get('/', function (req, res, next) {
+    res.render('carousel', { title: 'Grogan Burner Services' })
+  })
+
+  // Testing Flash messages
+  app.get('/flash', function (req, res) {
+    req.flash('info', 'Hi there!')
+    res.redirect('/')
+  })
+
+  app.get('/no-flash', function (req, res) {
+    res.redirect('/')
+  })
+
+  app.get('/multiple-flash', function (req, res) {
+    req.flash('info', ['Welcome', 'Please Enjoy'])
+    res.redirect('/')
+  })
 
   fs.readdirSync(__dirname).forEach(function (file) {
     if (file === 'index.js' || file.substr(file.lastIndexOf('.') + 1) !== 'js') {
