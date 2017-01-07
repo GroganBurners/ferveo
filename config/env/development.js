552437de943169813e1a5bd2f1acff74b05c7072
Neil Grogan <neil@grogan.ie>
2017-01-07 18:35:02 +0000

    Updated configs with defaults

--- config/env/development.js
+++ config/env/development.js
@@ -1,16 +1,21 @@
 module.exports = {
+  db: {
+    host: 'localhost',
+    port: 27017,
+    name: 'local'
+  },
+  baseUrl: '',
   google: {
     clientId: '',
     clientSecret: ''
   },
-  baseUrl: '',
   sauceLabs: {
     username: '',
     accessKey: ''
   },
   smtp: {
-    server: '',
-    port: '',
+    server: 'smtp.gmail.com',
+    port: 465,
     username: '',
     password: ''
   },
