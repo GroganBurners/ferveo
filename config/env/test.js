7adbbd074c0bdd25c2ce928df0ca794366a10086
Neil Grogan <neil@grogan.ie>
2017-01-07 18:31:18 +0000

    Updated config file with needed keys

--- config/env/test.js
+++ config/env/test.js
@@ -1,3 +1,21 @@
 module.exports = {
-
+  google: {
+    clientId: '',
+    clientSecret: ''
+  },
+  baseUrl: '',
+  sauceLabs: {
+    username: '',
+    accessKey: ''
+  },
+  smtp: {
+    server: '',
+    port: '',
+    username: '',
+    password: ''
+  },
+  sms: {
+    username: '',
+    password: ''
+  }
 }
