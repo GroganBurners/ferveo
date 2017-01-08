e5467de321a187ac041715619ac7c2251c798574
Neil Grogan <neil@grogan.ie>
2017-01-08 01:45:49 +0000

    Added logging to google auth

--- config/passport/google.js
+++ config/passport/google.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
 var GoogleStrategy = require('passport-google-oauth2').Strategy
 var User = mongoose.model('User')
 var config = require('../')
+var logger = require('winston')
 
 module.exports = new GoogleStrategy({
   clientID: config.google.clientId,
@@ -25,10 +26,11 @@ module.exports = new GoogleStrategy({
           google: profile._json
         })
         user.save(function (err) {
-          if (err) console.log(err)
+          logger.error('Error saving user: ' + err)
           return done(err, user)
         })
       } else {
+        logger.debug('Returning user : ' + user)
         return done(err, user)
       }
     })
