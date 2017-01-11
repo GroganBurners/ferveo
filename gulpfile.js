d51e80248aa88a6c2b3399ec40b6b3aea704c713
Neil Grogan <neil@grogan.ie>
2017-01-11 14:45:02 +0000

    Updated gulp script to minify images

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,23 @@
 var gulp = require('gulp')
 var browserSync = require('browser-sync')
 var nodemon = require('gulp-nodemon')
-const imagemin = require('gulp-imagemin');
- 
+const imagemin = require('gulp-imagemin')
+const mozjpeg = require('imagemin-mozjpeg')
+const pngquant = require('imagemin-pngquant')
+
 gulp.task('images', () =>
-      gulp.src('public/images/**/*')
-          .pipe(imagemin())
-          .pipe(gulp.dest('dist/images'))
+  gulp.src('public/images/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      use: [mozjpeg({
+                'quality' : '60',
+                'quantTable': 2
+              }),
+              pngquant({
+                'quality': '60'
+              })]
+    }))
+    .pipe(gulp.dest('dist/images'))
 );
 
 gulp.task('default', ['browser-sync'], function () {
@@ -28,8 +39,8 @@ gulp.task('nodemon', function (cb) {
     exec: 'node --debug',
     script: './bin/www'
   }).on('start', function () {
-		// to avoid nodemon being started multiple times
-		// thanks @matthisk
+    // to avoid nodemon being started multiple times
+    // thanks @matthisk
     if (!started) {
       cb()
       started = true
