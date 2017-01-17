4cd907937b10042d50213ce04697312568d0bb0f
Neil Grogan <neil@grogan.ie>
2017-01-17 22:07:13 +0000

    Add JWT as an auth strategy

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@
 var passport = require('passport')
 var mongoose = require('mongoose')
 var User = mongoose.model('User')
+var jwt = require('./passport/jwt')
 var local = require('./passport/local')
 var google = require('./passport/google')
 
@@ -10,6 +11,7 @@ passport.serializeUser((user, cb) => cb(null, user.id))
 passport.deserializeUser((id, cb) => User.load({ criteria: { _id: id } }, cb))
 
 // use these strategies
+passport.use(jwt)
 passport.use(local)
 passport.use(google)
 
---
+++ config/passport/jwt.js
@@ -0,0 +1,25 @@
+var JwtStrategy = require('passport-jwt').Strategy
+var ExtractJwt = require('passport-jwt').ExtractJwt
+var config = require('../')
+var mongoose = require('mongoose')
+var LocalStrategy = require('passport-local').Strategy
+var User = mongoose.model('User')
+
+module.exports = new JwtStrategy({
+        jwtFromRequest: ExtractJwt.fromAuthHeader(),
+        secretOrKey: config.secure.privateKey
+    }, (jwt_payload, done) => {
+    let opts = 
+    User.findOne({
+        id: jwt_payload.id
+    }, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    })
+})
