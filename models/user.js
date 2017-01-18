e8d27c2f78276d76274a686bb425872c11a0cd1d
Neil Grogan <neil@grogan.ie>
2017-01-18 19:53:03 +0000

    Added password hashing to user model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema
 
 var userSchema = new Schema({
   name: { type: String, default: '' },
-  email: { type: String, default: '' },
+  email: { type: String, required: true, unique: true, lowercase: true },
   username: { type: String, default: '' },
   provider: { type: String, default: '' },
   password: { type: String, default: '' },
@@ -13,6 +13,8 @@ var userSchema = new Schema({
   google: {}
 })
 
+
+// Statics are on Model class
 userSchema.statics = {
 
   /**
@@ -30,5 +32,36 @@ userSchema.statics = {
       .exec(cb)
   }
 }
+
+// Method on user objects to compare password input to password saved
+userSchema.methods.comparePassword = function(pw, cb) {
+  bcrypt.compare(pw, this.password, function(err, isMatch) {
+    if (err) {
+      return cb(err)
+    }
+    cb(null, isMatch)
+  })
+}
+
+// Hash user password before saving new user
+userSchema.pre('save', function(next) {
+  var user = this
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) {
+        return next(err)
+      }
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          return next(err)
+        }
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    return next()
+  }
+})
 
 module.exports = mongoose.model('User', userSchema)
