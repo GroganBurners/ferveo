5cba70e0f0dd2c26747a9ee9ccb321bc241bb76a
Neil Grogan <neil@grogan.ie>
2017-08-11 15:17:46 +0100

    Added site info to template helpers
      For Pug Templates:
      * Adds full address
      * Adds social links
      * Adds full site name

--- middlewares/helper.js
+++ middlewares/helper.js
@@ -1,19 +1,30 @@
-exports.moment = require("moment");
+exports.moment = require('moment');
 
-exports.siteName = "Grogan Burner Services";
+exports.siteName = 'Grogan Burner Services';
+
+exports.siteUrl = 'https://www.groganburners.ie';
+
+exports.addressStreet = 'Ballyda';
+exports.addressTown = 'Danesfort';
+exports.addressCounty = 'Kilkenny';
+exports.addressPostcode = 'R95 CC92';
+exports.addressCountry = 'Ireland';
+
+exports.socialFacebook = 'https://www.facebook.com/groganburners';
+exports.socialTwitter = 'https://www.twitter.com/groganburners';
 
 exports.menu = [
-  { slug: "/", title: "Home" },
-  { slug: "/#gas", title: "Gas Services" },
-  { slug: "/#oil", title: "Oil Services" },
-  { slug: "/#repair", title: "Boiler Repair" },
-  { slug: "/#contact", title: "Contact Us" },
-  { slug: "/#about", title: "About Us" }
+  {slug: '/', title: 'Home'},
+  {slug: '/#gas', title: 'Gas Services'},
+  {slug: '/#oil', title: 'Oil Services'},
+  {slug: '/#repair', title: 'Boiler Repair'},
+  {slug: '/#contact', title: 'Contact Us'},
+  {slug: '/#about', title: 'About Us'}
 ];
 
 exports.adminMenu = [
-  { slug: "/", title: "Home" },
-  { slug: "/customers", title: "Customers" },
-  { slug: "/expenses", title: "Expenses" },
-  { slug: "/invoices", title: "Invoices" }
+  {slug: '/', title: 'Home'},
+  {slug: '/customers', title: 'Customers'},
+  {slug: '/expenses', title: 'Expenses'},
+  {slug: '/invoices', title: 'Invoices'}
 ];
